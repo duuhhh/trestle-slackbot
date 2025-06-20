87a14a81f4864e2ff6ef3920877efff485d9bf2a
Alvaro Hernandez <Alvaroh8@nycstudents.net>
2025-06-20 19:33:29 -0400

    fixed command

--- src/commands.ts
+++ src/commands.ts
@@ -1,5 +1,6 @@
 import { App, AckFn, RespondArguments, SayFn, SlashCommand } from "@slack/bolt";
 
+import { RespondFn } from "@slack/bolt";
 import { MessageError } from "./errors";
 import { ChatBot } from "./types";
 import { getFaceQuiz } from "./quiz";
@@ -12,18 +13,23 @@ const getFaceQuizCommand =
     command,
     ack,
     say,
+    respond,
   }: {
     command: SlashCommand;
-    ack: AckFn<string | RespondArguments>;
+    respond: RespondFn;
+    ack: AckFn<string>;
     say: SayFn;
   }) => {
+    await ack();
+    await respond("Thanks! Processing your face quiz...");
+
     try {
-      await ack();
       const slackUsers = await fetchUsers({ app });
       const quiz = await getFaceQuiz({
         exclude: [command.user_id],
         slackUsers,
       });
+
       await app.dm({ user: command.user_id, blocks: quiz.blocks });
     } catch (error) {
       if (error instanceof MessageError) {
